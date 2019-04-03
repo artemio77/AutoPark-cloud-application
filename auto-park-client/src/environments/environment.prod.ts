cb1a2db20fc0d6ea4d1188d27355381f4f81d177
Artem <derevets.artem@gmail.com>
2019-04-03 15:25:56 +0300

    Client Modification

--- auto-park-client/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  cassandraServerUrl: 'https://oauth-resource-server-1.herokuapp.com',
-  apiUrl: 'https://oauth-autorization-server.herokuapp.com'
-};
--- auto-park-client/src/model/route.ts
+++
@@ -1,9 +0,0 @@
-import {User} from './user';
-import {TransportEntity} from './transport';
-
-export class RouteEntity {
-  id: string;
-  name: string;
-  routeNumber: string;
-  transportList: TransportEntity[];
-}
